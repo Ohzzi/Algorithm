256e1238f61fccf9c641bb109e784be8887d1ca7
Ohzzi <clover7even@naver.com>
2020-08-24 15:20:29 +0900

    BOJ#2751

---
+++ BOJ/2751/2751.cpp
@@ -0,0 +1,24 @@
+/*
+문제
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+입력
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다.
+이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+*/
+
+#include <iostream>
+#include <algorithm>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	std::sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", arr[i]);
+	}
+	return 0;
+}
