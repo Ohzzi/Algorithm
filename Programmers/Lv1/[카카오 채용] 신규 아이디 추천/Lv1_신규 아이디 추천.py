0cb77af7a4cde80d47cb013679889af678dcab45
Ohzzi <clover7even@naver.com>
2021-06-29 20:41:38 +0900

    신규 아이디 추천_개선된 풀이

--- Programmers/Lv1/[카카오 채용] 신규 아이디 추천/Lv1_신규 아이디 추천.py
+++ Programmers/Lv1/[카카오 채용] 신규 아이디 추천/Lv1_신규 아이디 추천.py
@@ -33,3 +33,32 @@ def solution(new_id):
         answer_list.append(answer_list[len(answer_list) - 1])
     answer = "".join(answer_list)
     return answer
+
+def better_solution(new_id):
+    answer = ''
+    # 1단계
+    new_id = new_id.lower()
+    # 2단계
+    for c in new_id:
+        if c.isalpha() or c.isdigit() or c in ['-', '_', '.']:
+            answer += c
+    # 3단계
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    # 4단계
+    if answer[0] == '.' and len(answer) > 1:
+        answer = answer[1:]
+    if answer[-1] == '.':
+        answer = answer[:-1]
+    # 5단계
+    if answer == '':
+        answer = 'a'
+    # 6단계
+    if len(answer) > 15:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    # 7단계
+    while len(answer) < 3:
+        answer += answer[-1]
+    return answer
